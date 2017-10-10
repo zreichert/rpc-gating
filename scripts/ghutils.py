2268963201808b35f082253ee5554aba77a27149
git-harry <git-harry@live.co.uk>
2017-10-10 22:55:02 +0100

    Publish tag before generating release notes
    This change addresses some issues with generating release notes:
    - notes were being generated from a ref (branch), there was nothing to
      guarantee this did not change when running the release code
    - rpc-differ, and reno when support is added in rpc-openstack, require
      the tag name to produce correctly worded notes
    
    To ensure the tag is created at the start of the process, a new
    function `publish_tag` is added to handle pushing the tag to the
    repository. This removes that burden from `create_release` with the
    added benefit that new tags are annotated (GitHub only creates
    lightweight tags).
    
    To enable the tag to be pushed the job now provides access to the
    Jenkins GitHub SSH key.
    
    `generate_release_notes` now sets `RE_HOOK_VERSION` to be the version
    and not the ref, this is both more intuitive and means that the tag is
    now available by the consumer project scripts.

--- scripts/ghutils.py
+++ scripts/ghutils.py
@@ -235,17 +235,12 @@ def update_rc_branch(ctx, mainline, rc):
     help="Symbolic name of Release (eg r14.1.99)"
 )
 @click.option(
-    '--ref',
-    help="Reference to create release from (branch, SHA etc)"
-         " May be omitted if supplied to clone earlier in the chain."
-)
-@click.option(
     '--bodyfile',
     help="File containing release message body",
     required=True
     # Can't use type=click.File because the file may not exist on startup
 )
-def create_release(repo, version, ref, bodyfile):
+def create_release(repo, version, bodyfile):
     ctx_obj = click.get_current_context().obj
     # Attempt to read release_notes from context
     # They may have been set by release.generate_release_notes
@@ -258,8 +253,9 @@ def create_release(repo, version, ref, bodyfile):
         ))
         click.get_current_context().exit(-1)
 
-    ref = try_context(ctx_obj, ref, "ref", "rc_ref")
+    version = try_context(ctx_obj, version, "version", "version")
     # Store version in context for use in notifications
+    version = try_context(ctx_obj, version, "version", "version")
     ctx_obj.version = version
     # Create a subject for use by notifications
     ctx_obj.release_subject = "Version {v} of {o}/{r} released".format(
@@ -270,7 +266,7 @@ def create_release(repo, version, ref, bodyfile):
     try:
         repo.create_release(
             version,            # tag name
-            ref,                # tag reference
+            version,            # tag reference
             version,            # release name
             release_notes       # release body
         )
@@ -301,7 +297,7 @@ def create_release(repo, version, ref, bodyfile):
             "+refs/heads/*:refs/heads/*")
 def clone(url, ref, refspec):
     ctx_obj = click.get_current_context().obj
-    url = try_context(ctx_obj, url, "url", "clone_url")
+    url = try_context(ctx_obj, url, "url", "ssh_url")
     ctx_obj.rc_ref = ref
     clone_dir = "{o}/{r}".format(
         o=ctx_obj.owner.login,
