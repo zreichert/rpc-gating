758d3198f7bbc3bf8c2dcf4c50dc2e088b0d9973
git-harry <git-harry@live.co.uk>
2017-11-16 14:24:47 +0000

    Remove use of `dir` from artefact uploads

--- pipeline_steps/common.groovy
+++ pipeline_steps/common.groovy
@@ -336,9 +336,7 @@ def rpco_archive_artifacts(String build_type = "AIO"){
 def archive_artifacts(){
   stage('Compress and Publish Artifacts'){
     if (env.RE_HOOK_RESULT_DIR != null){
-      dir(env.RE_HOOK_RESULT_DIR){
-        junit allowEmptyResults: true, testResults: '*.xml'
-      }
+      junit allowEmptyResults: true, testResults: "${env.RE_HOOK_RESULT_DIR}/*.xml"
     }
     pubcloud.uploadToSwift(
       container: "jenkins_logs",
